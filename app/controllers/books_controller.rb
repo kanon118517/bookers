59f43e68cfad980484a3f630f885a1c42e8bc9c7
kanon118517 <124481925+kanon118517@users.noreply.github.com>
2023-03-21 07:50:32 +0000

    [Update]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,12 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -27,6 +30,12 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
   private
   
   def book_params
@@ -34,3 +43,4 @@ class BooksController < ApplicationController
   end
   
 end
+
