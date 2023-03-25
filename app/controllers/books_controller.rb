34f703b8de24965060c3490720a6ec01f14e5902
kanon118517 <124481925+kanon118517@users.noreply.github.com>
2023-03-25 12:26:10 +0000

    [Update]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "book was successfully created."
       redirect_to book_path(@book.id)
     else
       render :new
@@ -14,6 +15,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
